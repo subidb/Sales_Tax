0d79f70e83d7722819cc6dc9423888c42024bf1c
Subid <basaulasubid1@gmail.com>
2022-01-11 21:05:29 +0545

    added class functions for tax calculation, program complete

--- Item.py
+++ Item.py
@@ -1,3 +1,5 @@
+import math
+
 class Item: #Item class with all the attributes
     def __init__(self, name, quantity, price, tax=0, price_after_tax=0, excempt=False, imported=False):
         self.name = name
@@ -11,3 +13,24 @@ class Item: #Item class with all the attributes
     def __repr__(self):
         return "\n[name:{}, quantity:{}, price:{}, excempt:{}, imported:{}, tax:{:.2f}, price_after_tax:{:.2f}]"\
             .format(self.name, self.quantity, self.price, self.excempt, self.imported, self.tax, self.price_after_tax) 
+            
+            
+    def sales_tax(self): #sets sales tax percentage on the item based on the given information
+        
+        if self.excempt == False and self.imported == False:
+            self.tax_rate = 0.1 
+            
+        if self.excempt == False and self.imported == True:
+            self.tax_rate = 0.15
+            
+        if self.excempt == True and self.imported == False:
+            self.tax_rate = 0
+            
+        if self.excempt == True and self.imported == True:
+            self.tax_rate = 0.05 
+            
+            
+    def tax_calc(self): #calculates the tax on the good based on the information 
+        self.tax = self.tax_rate * self.price
+        self.tax = math.ceil(self.tax / 0.05) * .05
+        self.price_after_tax  = self.price + self.tax
--- sales_tax.py
+++ sales_tax.py
@@ -2,12 +2,6 @@ import re
 from Item import *
 
 
-#inputitemlist     
-itemlist1 = ['1 book at 12.49','1 music CD at 14.99', '1 chocolate bar at 0.85'] 
-itemlist2 = ['1 imported box of chocolates at 10.00', '1 imported bottle of perfume at 47.50'] 
-itemlist3 = ['1 imported bottle of perfume at 27.99', '1 bottle of perfume at 18.99',
-             '1 packet of headache pills at 9.75', '1 box of imported chocolates at 11.25']
-
 def parser(itemlist): #parses the input of the goods and converts the items into python objects with properties/attributes/values
     item_objs_list = [] 
     excempt_prods = ["chocolate","book", "tablets", "pills"]     #list of products that are except from basic sales tax
@@ -31,5 +25,42 @@ def parser(itemlist): #parses the input of the goods and converts the items into
         
     return item_objs_list #returns the list of item objects after parsing
 
-objslist = parser(itemlist1)
-print(objslist)
+#inputitemlist     
+itemlist1 = ['1 book at 12.49','1 music CD at 14.99', '1 chocolate bar at 0.85'] 
+itemlist2 = ['1 imported box of chocolates at 10.00', '1 imported bottle of perfume at 47.50'] 
+itemlist3 = ['1 imported bottle of perfume at 27.99', '1 bottle of perfume at 18.99',
+             '1 packet of headache pills at 9.75', '1 box of imported chocolates at 11.25']
+
+inputlist = [itemlist1, itemlist2, itemlist3] 
+
+output_receipt = []          
+        
+for count, itemlist in enumerate(inputlist): 
+    
+    output_receipt.append("\nOutput {}:\n".format(count+1))
+    item_objs_list = parser(itemlist) 
+
+    if type(item_objs_list) is list: #item_objs_list is a list only if the input is valid
+        for item in item_objs_list:
+            item.sales_tax() 
+            item.tax_calc() #member functions of the Item Class to calculate sales tax
+            sales_taxes_sum = 0
+            total = 0
+            
+        for item in item_objs_list: #adding up the sales tax sum and total for each item list, adding them to the output list
+            
+            sales_taxes_sum+=item.tax
+            total+= item.price_after_tax
+            output_receipt.append(("{}{}: {:.2f}".format(item.quantity, item.name, item.price_after_tax)))
+        
+        output_receipt.append(("Sales Taxes:{:.2f}".format(sales_taxes_sum)))
+        output_receipt.append(("Total:{:.2f}".format(total)))
+        output_receipt.append("_______________________________________")
+            
+        
+    else:
+        output_receipt.append("\nInput error, could not process")
+        output_receipt.append("_______________________________________")
+
+for receipt in output_receipt:
+    print(receipt)
